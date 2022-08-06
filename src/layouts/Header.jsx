a30575aec3461580cfb26a7cd9f14af8fe2e998a
Ali-Eldeba <alieldeba375@gmail.com>
2022-08-06 06:48:23 +0200

    adding header

---
+++ src/layouts/Header.jsx
@@ -0,0 +1,28 @@
+export default function Header() {
+  return (
+    <header className="bg-white">
+      <div className="container">
+        <div className="flex justify-between items-center h-[56px] relative">
+          <div className="user">
+            <img
+              src="https://universeofmemory.com/wp-content/uploads/2020/06/Man-Placeholder.png"
+              alt="user"
+              className="rounded-full w-[50px] cursor-pointer"
+            />
+            <ul className="list absolute top-[90%] hidden opacity-0 z-5 transition-all bg-white py-3 px-3">
+              <li className="cursor-pointer">Profile</li>
+              <li className="cursor-pointer">Favourits</li>
+              <li className="cursor-pointer">Settings</li>
+              <li className="cursor-pointer">Log out</li>
+            </ul>
+          </div>
+          <ul className="flex items-center gap-5 h-full">
+            <li className="cursor-pointer">Games</li>
+            <li className="cursor-pointer">Sign up</li>
+            <li className="cursor-pointer">Sign in</li>
+          </ul>
+        </div>
+      </div>
+    </header>
+  );
+}
