e016721f80bfc1d131315d4cc70e07429b4f08b1
alieldeba <alieldeba375@gmail.com>
2022-08-08 02:32:31 +0200

    adding fake api

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,20 @@
+import {useEffect} from "react"
 import { Routes, Route } from "react-router-dom";
+import axios from "axios";
 import Header from "./layouts/Header";
 import Home from "./pages/Home";
 import NotFound from "./pages/NotFound";
 
 export default function App() {
+  
+  useEffect(() => {
+    axios
+      .get("https://my-json-server.typicode.com/alieldeba/wifi4games/results/")
+      .then((response) => {
+        console.log(response.data);
+      });
+  }, []);
+
   return (
     <>
       <Header />
