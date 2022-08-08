5aa851835df7cf72ee6d4dd27dade2e707368199
alieldeba <alieldeba375@gmail.com>
2022-08-09 00:26:20 +0200

    adding new games

--- src/components/GameCard.jsx
+++ src/components/GameCard.jsx
@@ -1,11 +1,11 @@
 export default function GameCard(props) {
   return (
-    <article className="max-w-md mx-auto mt-4 shadow-lg border rounded-md duration-300 hover:shadow-sm">
+    <article className="max-w-md mx-auto mt-4 shadow-lg border rounded-md duration-300 hover:shadow-sm w-[340px]">
       <img
         src={props.data.image}
         loading="lazy"
         alt={props.data.title}
-        className="w-full h-48 rounded-t-md"
+        className="w-full h-48 rounded-t-md object"
       />
       <div className="flex items-center mt-2 pt-3 ml-4 mr-2"></div>
       <div className="pt-3 ml-4 mr-2 mb-3">
