49ef73136cc27b1c66dc567134d3ea0f20c305e1
alieldeba <alieldeba375@gmail.com>
2022-08-10 11:00:53 +0200

    changing the header

---
+++ src/components/ProfileDropDown.jsx
@@ -0,0 +1,57 @@
+import { useState, useRef, useEffect } from "react";
+
+export default function ProfileDropDown(props) {
+  const [state, setState] = useState(false);
+  const profileRef = useRef();
+
+  const navigation = [
+    { title: "Favorites", path: "javascript:void(0)" },
+    { title: "Settings", path: "javascript:void(0)" },
+    { title: "Log out", path: "javascript:void(0)" },
+  ];
+
+  useEffect(() => {
+    const handleDropDown = (e) => {
+      if (!profileRef.current.contains(e.target)) setState(false);
+    };
+    document.addEventListener("click", handleDropDown);
+  }, []);
+
+  return (
+    <div className={`relative ${props.class}`}>
+      <div className="flex items-center space-x-4">
+        <button
+          ref={profileRef}
+          className="w-10 h-10 outline-none rounded-full ring-offset-2 ring-gray-200 ring-2 lg:focus:ring-indigo-600"
+          onClick={() => setState(!state)}
+        >
+          <img
+            src="https://png.pngitem.com/pimgs/s/506-5067022_sweet-shap-profile-placeholder-hd-png-download.png"
+            className="w-full h-full rounded-full"
+          />
+        </button>
+        <div className="lg:hidden">
+          <span className="block">Micheal John</span>
+          <span className="block text-sm text-gray-500">john@gmail.com</span>
+        </div>
+      </div>
+      <ul
+        className={`bg-white top-12 right-0 mt-5 space-y-5 lg:absolute lg:border lg:rounded-md lg:text-sm lg:w-52 lg:shadow-md lg:space-y-0 lg:mt-0 ${
+          state ? "" : "lg:hidden"
+        }`}
+      >
+        {navigation.map((item, idx) => (
+          <li>
+            <a
+              key={idx}
+              className="block text-gray-600 lg:hover:bg-gray-50 lg:p-2.5"
+              href={item.path}
+            >
+              {item.title}
+            </a>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
--- src/layouts/Header.jsx
+++ src/layouts/Header.jsx
@@ -1,37 +1,140 @@
+import { useState, useRef, useEffect } from "react";
 import { Link } from "react-router-dom";
+import ProfileDropDown from "../components/ProfileDropDown";
 
 export default function Header() {
+  // return (
+  // <header className="bg-white drop-shadow-sm">
+  //   <div className="container">
+  //     <div className="flex justify-between items-center h-[56px] relative">
+  //       <div className="user">
+  //         <img
+  //           src="https://universeofmemory.com/wp-content/uploads/2020/06/Man-Placeholder.png"
+  //           alt="user"
+  //           className="rounded-full w-[50px] cursor-pointer"
+  //         />
+  //         <ul className="list absolute top-[90%] hidden opacity-0 z-5 transition-all bg-white py-3 px-3">
+  //           <li className="cursor-pointer">Profile</li>
+  //           <li className="cursor-pointer">Favourits</li>
+  //           <li className="cursor-pointer">Settings</li>
+  //           <li className="cursor-pointer">Log out</li>
+  //         </ul>
+  //       </div>
+  //       <ul className="flex items-center h-full">
+  //         <li className="cursor-pointer mr-5">
+  //           <Link to="/">Home</Link>
+  //         </li>
+  //         <li className="cursor-pointer mr-5">
+  //           <Link to="/games">Games</Link>
+  //         </li>
+  //         <li className="cursor-pointer">
+  //           <Link to="/signin">Sign in</Link>
+  //         </li>
+  //       </ul>
+  //     </div>
+  //   </div>
+  // </header>
+  // );
+
+  const [menuState, setMenuState] = useState(false);
+
+  // Replace javascript:void(0) path with your path
+  const navigation = [
+    { title: "Home", path: "/" },
+    { title: "Games", path: "/games" },
+    { title: "Sign in", path: "/signin" },
+  ];
   return (
-    <header className="bg-white drop-shadow-sm">
-      <div className="container">
-        <div className="flex justify-between items-center h-[56px] relative">
-          <div className="user">
+    <nav className="bg-white border-b">
+      <div className="flex items-center space-x-8 py-3 px-4 max-w-screen-xl mx-auto">
+        <div className="flex-none lg:flex-initial">
+          <Link to="/">
             <img
-              src="https://universeofmemory.com/wp-content/uploads/2020/06/Man-Placeholder.png"
-              alt="user"
-              className="rounded-full w-[50px] cursor-pointer"
+              src="https://www.wifi4games.com/css/img/logo_md.png"
+              width={50}
+              height={10}
+              alt="Float UI logo"
             />
-            <ul className="list absolute top-[90%] hidden opacity-0 z-5 transition-all bg-white py-3 px-3">
-              <li className="cursor-pointer">Profile</li>
-              <li className="cursor-pointer">Favourits</li>
-              <li className="cursor-pointer">Settings</li>
-              <li className="cursor-pointer">Log out</li>
+          </Link>
+        </div>
+        <div className="flex-1 flex items-center justify-between">
+          <div
+            className={`bg-white absolute z-20 w-full top-16 left-0 p-4 border-b lg:static lg:block lg:border-none ${
+              menuState ? "" : "hidden"
+            }`}
+          >
+            <ul className="mt-12 space-y-5 lg:flex lg:space-x-6 lg:space-y-0 lg:mt-0">
+              {navigation.map((item, idx) => (
+                <li key={idx} className="text-gray-600 hover:text-gray-900">
+                  <Link to={item.path}>{item.title}</Link>
+                </li>
+              ))}
             </ul>
+            <ProfileDropDown class="mt-5 pt-5 border-t lg:hidden" />
           </div>
-          <ul className="flex items-center h-full">
-            <li className="cursor-pointer mr-5">
-              <Link to="/">Home</Link>
-            </li>
-            <li className="cursor-pointer mr-5">
-              <Link to="/games">Games</Link>
-            </li>
-            <li className="cursor-pointer">
-              <Link to="/signin">Sign in</Link>
-            </li>
-          </ul>
+          <div className="flex-1 flex items-center justify-end space-x-2 sm:space-x-6">
+            <form className="flex items-center space-x-2 border rounded-md p-2">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5 flex-none text-gray-300"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth={2}
+                  d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+                />
+              </svg>
+              <input
+                className="w-full outline-none appearance-none placeholder-gray-500 text-gray-500 sm:w-auto"
+                type="text"
+                placeholder="Search"
+              />
+            </form>
+            <ProfileDropDown class="hidden lg:block" />
+            <button
+              className="outline-none text-gray-400 block lg:hidden"
+              onClick={() => setMenuState(!menuState)}
+            >
+              {menuState ? (
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-6 w-6"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth={2}
+                    d="M6 18L18 6M6 6l12 12"
+                  />
+                </svg>
+              ) : (
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-6 w-6"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth={2}
+                    d="M4 6h16M4 12h16m-7 6h7"
+                  />
+                </svg>
+              )}
+            </button>
+          </div>
         </div>
       </div>
-    </header>
+    </nav>
   );
 }
 
